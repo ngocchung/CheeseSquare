e235dda79e1d7c67c0ebb7b9f13594b6c9a9738a
ngocchung <phc247@outlook.com>
2015-12-26 15:23:47 +0700

    Cheesesquare Sample - Customized Version
    Cheesesquare Sample - Customized Version

---
+++ README.md
@@ -0,0 +1,79 @@
+![alt text](https://raw.githubusercontent.com/chrisbanes/cheesesquare/master/art/icon.png)
+
+Cheesesquare Sample - Customized Version
+===================================
+
+Demos the new Android Design library. This is not an exhaustive sample, but shows
+some of the important features in the Design library:
+
+- Collapsing Toolbar
+- FloatingActionButton
+- View anchoring
+- NavigationView
+- Snackbar
+
+Pre-requisites
+--------------
+
+- Android SDK v22
+- Android Build Tools v22.0.1
+- Android Support Repository r16 (for v22.2.1)
+
+Customized Contents:
+
+```
+		final ViewGroup.LayoutParams toolbarLayoutParams = toolbar.getLayoutParams();
+        if (toolbarLayoutParams != null) {
+            toolbarHeight = toolbarLayoutParams.height;
+        }
+
+        final CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(cheeseName);
+
+        screenHeight = getScreenHeight(this);
+
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout1);
+        ViewTreeObserver observer = linearLayout.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                linearLayoutHeight = linearLayout.getHeight();
+                if (linearLayoutHeight + toolbarHeight < screenHeight) {
+                    if (toolbarLayoutParams != null) {
+                        toolbarLayoutParams.height = screenHeight - linearLayoutHeight - 20;                        
+                        collapsingToolbar.setContentScrimColor(ContextCompat.getColor(mContext, android.R.color.transparent));
+                    }
+                }
+                // Removes the listener if possible
+                ViewTreeObserver viewTreeObserver = linearLayout.getViewTreeObserver();
+                if (viewTreeObserver.isAlive()) {
+                    if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                        linearLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    } else {
+                        linearLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+                }
+            }
+        });
+```
+
+License
+-------
+
+Copyright 2014 The Android Open Source Project, Inc.
+
+Licensed to the Apache Software Foundation (ASF) under one or more contributor
+license agreements.  See the NOTICE file distributed with this work for
+additional information regarding copyright ownership.  The ASF licenses this
+file to you under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License.  You may obtain a copy of
+the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+License for the specific language governing permissions and limitations under
+the License.
---
+++ app/src/main/java/com/support/android/designlibdemo/CheeseDetailActivity.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.support.android.designlibdemo;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Point;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.Menu;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bumptech.glide.Glide;
+
+public class CheeseDetailActivity extends AppCompatActivity {
+
+    public static final String EXTRA_NAME = "cheese_name";
+    private final Context mContext = this;
+    private int screenHeight;
+    private int linearLayoutHeight;
+    private int toolbarHeight;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        final String cheeseName = intent.getStringExtra(EXTRA_NAME);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final ViewGroup.LayoutParams toolbarLayoutParams = toolbar.getLayoutParams();
+        if (toolbarLayoutParams != null) {
+            toolbarHeight = toolbarLayoutParams.height;
+        }
+
+        final CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(cheeseName);
+
+        screenHeight = getScreenHeight(this);
+
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout1);
+        ViewTreeObserver observer = linearLayout.getViewTreeObserver();
+        observer.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                linearLayoutHeight = linearLayout.getHeight();
+                if (linearLayoutHeight + toolbarHeight < screenHeight) {
+                    if (toolbarLayoutParams != null) {
+                        toolbarLayoutParams.height = screenHeight - linearLayoutHeight - 20;
+                        // collapsingToolbar.setContentScrimColor(getResources().getColor(android.R.color.transparent));
+                        collapsingToolbar.setContentScrimColor(ContextCompat.getColor(mContext, android.R.color.transparent));
+                    }
+                }
+                // Removes the listener if possible
+                ViewTreeObserver viewTreeObserver = linearLayout.getViewTreeObserver();
+                if (viewTreeObserver.isAlive()) {
+                    if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                        linearLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    } else {
+                        linearLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                    }
+                }
+            }
+        });
+
+        loadBackdrop();
+    }
+
+    private int getScreenHeight(Context context) {
+        int measuredHeight;
+        Point size = new Point();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            wm.getDefaultDisplay().getSize(size);
+            measuredHeight = size.y;
+        } else {
+            Display d = wm.getDefaultDisplay();
+            measuredHeight = d.getHeight();
+        }
+
+        return measuredHeight;
+    }
+
+    private void loadBackdrop() {
+        final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
+        Glide.with(this).load(Cheeses.getRandomCheeseDrawable()).centerCrop().into(imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.sample_actions, menu);
+        return true;
+    }
+}
